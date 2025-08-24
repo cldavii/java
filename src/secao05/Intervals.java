df930e1c139c70e16f367584ddb6e9498e05e156
cldavii <davifsc03@gmail.com>
2025-08-23 22:10:58 -0300

    Exercício para verificar em que intervalo está o número

---
+++ src/secao05/Intervals.java
@@ -0,0 +1,22 @@
+package secao05;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Intervals {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        float value = scanner.nextFloat();
+        if (value >= 0 && value <= 25) {
+            System.out.println("Intervalo [0,25]");
+        } else if (value > 25 && value <= 50) {
+            System.out.println("Intervalo [25,50]");
+        } else if (value > 75 && value <= 100) {
+            System.out.println("Intervalo [75,100]");
+        } else {
+            System.out.println("Fora de intervalo");
+        }
+        scanner.close();
+    }
+}
