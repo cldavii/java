9075b4ec15bd818a79ab2419d1d65591803ae509
cldavii <davifsc03@gmail.com>
2025-08-27 03:13:50 -0300

    Exercício para listar o número, seu valor quadrático e cúbico

--- src/secao06/ForExs/Exponential.java
+++ src/secao06/ForExs/Exponential.java
@@ -1,4 +1,15 @@
 package secao06.ForExs;
 
+import java.util.Scanner;
+
 public class Exponential {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        byte n = scanner.nextByte();
+        for (int i = 1; i <= n; i++) {
+            System.out.println(i + " " + (int)Math.pow(i, 2) + " " + (int)Math.pow(i, 3));
+        }
+        scanner.close();
+    }
 }
