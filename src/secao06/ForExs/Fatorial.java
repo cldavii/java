56a05ec3b57f8fba585fc99b02bf18aea93626b1
cldavii <davifsc03@gmail.com>
2025-08-27 03:14:17 -0300

    Exercício para calcular o fatorial de uum número

--- src/secao06/ForExs/Fatorial.java
+++ src/secao06/ForExs/Fatorial.java
@@ -1,4 +1,17 @@
 package secao06.ForExs;
 
+import java.util.Scanner;
+
 public class Fatorial {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        byte n = scanner.nextByte();
+        int fatorial = 1;
+        for (byte i = n; i >= 1; i--) {
+            fatorial *= i;
+        }
+        System.out.println(fatorial);
+        scanner.close();
+    }
 }
