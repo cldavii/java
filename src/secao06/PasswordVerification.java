6e8bf278c38f315e64da5cb7a5e0a1dc829b216b
cldavii <davifsc03@gmail.com>
2025-08-27 00:14:23 -0300

    Alteração do tipo da variável tryPassword de String para short

--- src/secao06/PasswordVerification.java
+++ src/secao06/PasswordVerification.java
@@ -7,11 +7,11 @@ public class PasswordVerification {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-        String password = "2002";
-        String tryPassword = scanner.next();
-        while (!tryPassword.equals(password)) {
+        short password = 2002;
+        short tryPassword = scanner.nextShort();
+        while (tryPassword != password) {
             System.out.println("Senha Invalida");
-            tryPassword = scanner.next();
+            tryPassword = scanner.nextShort();
         }
         System.out.println("Acesso Permitido");
         scanner.close();
