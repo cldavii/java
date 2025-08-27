8d1deb12e1a66ae91f44488a0fefda5afd5303b5
cldavii <davifsc03@gmail.com>
2025-08-27 03:16:22 -0300

    Exercício para calcular a média ponderada

--- src/secao06/ForExs/WeightedAverage.java
+++ src/secao06/ForExs/WeightedAverage.java
@@ -1,4 +1,22 @@
 package secao06.ForExs;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class WeightedAverage {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        byte n = scanner.nextByte();
+        float a, b, c;
+        float average;
+        for (int i = 0; i < n; i++) {
+            a = scanner.nextFloat();
+            b = scanner.nextFloat();
+            c = scanner.nextFloat();
+            average = (a * 2 + b * 3 + c * 5) / (2 + 3 + 5);
+            System.out.printf("%.1f%n", average);
+        }
+        scanner.close();
+    }
 }
