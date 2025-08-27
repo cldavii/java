bd512dbc1c6088ae96840b253e636242dde71dbe
cldavii <davifsc03@gmail.com>
2025-08-27 01:31:03 -0300

    Exercício para listar apenas os números ímpares

--- src/secao06/ForExs/OddNumbers.java
+++ src/secao06/ForExs/OddNumbers.java
@@ -1,4 +1,20 @@
 package secao06.ForExs;
 
+import java.util.Scanner;
+
 public class OddNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        short x = scanner.nextShort();
+        if (x < 1 || x > 1000) {
+            System.out.println("Invalid number");
+        }
+        for (int i = 0; i < x; i++) {
+            if (i % 2 == 1) {
+                System.out.println(i);
+            }
+        }
+        scanner.close();
+    }
 }
