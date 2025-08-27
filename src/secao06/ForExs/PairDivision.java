82358df7e2448d249c1cff342379eb40aaf88156
cldavii <davifsc03@gmail.com>
2025-08-27 03:15:33 -0300

    Exercício para calcular uma divisão

--- src/secao06/ForExs/PairDivision.java
+++ src/secao06/ForExs/PairDivision.java
@@ -1,4 +1,22 @@
 package secao06.ForExs;
 
+import java.util.Scanner;
+
 public class PairDivision {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        byte n = scanner.nextByte();
+        for (int i = 1; i <= n; i++) {
+            byte numerator = scanner.nextByte();
+            byte denominator = scanner.nextByte();
+            if (denominator == 0) {
+                System.out.println("divisao impossivel");
+            } else {
+                double division = (double) numerator / denominator;
+                System.out.println(division);
+            }
+        }
+        scanner.close();
+    }
 }
